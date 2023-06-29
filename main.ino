f33871b5a74519fd48ecd1928edb1a543cef4b2c
John Bouyer <109704002+Turfader@users.noreply.github.com>
2023-06-28 22:54:44 -0700

    Created main.ino
    This is the main arduino file

---
+++ main.ino
@@ -0,0 +1,9 @@
+// The main file that the arduino will run off of. Keep this file clean. Don't push anything broken to this file plz. Leave comments plz.
+void setup()
+{
+  
+}
+void loop()
+{
+  
+}
