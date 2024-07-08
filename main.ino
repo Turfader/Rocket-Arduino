5b409f3d416e7a9ed7945abc2969ea072c5f94cc
John Bouyer <109704002+Turfader@users.noreply.github.com>
2024-07-07 22:53:04 -0700

    Decided to actually make it work
    I also moved to hardware safety instead of software safety

--- main.ino
+++ main.ino
@@ -1,58 +1,81 @@
-// C++ code
-//
+#include <SoftwareSerial.h>
+#include "Arduino.h"
 
-#define LED_SAFETY 12
-#define SAFETY_OUT 8
-#define SAFETY_IN 7
+const int transistorPin1 = 3;
+const int transistorPin2 = 4;
+const int transistorPin3 = 5;
 
-#define LED_SWITCH 11
-#define SWITCH_OUT 6
-#define SWITCH_IN 5
+const byte transmit = 13; //tx
+const byte receive = 12; //rx
 
-#define LED_READY_TO_FIRE 0
+SoftwareSerial BTSerial(receive, transmit); //receive, transmit
 
-void setup()
-{
-	Serial.begin(9600);
- 	Serial.println();
-	pinMode(LED_SAFETY, OUTPUT);
- 	pinMode(SAFETY_OUT, OUTPUT);
-	pinMode(SAFETY_IN, INPUT);
-  
-  	pinMode(LED_SWITCH, OUTPUT);
-  	pinMode(SWITCH_OUT, OUTPUT);
-  	pinMode(SWITCH_IN, INPUT);
-  
-  	pinMode(LED_READY_TO_FIRE, OUTPUT);
+void setup() {
+  pinMode(transmit, OUTPUT);
+  pinMode(receive, INPUT);
+
+  BTSerial.begin(9600);
+  Serial.begin(9600);
+  Serial.println("\n--------------------");
+  Serial.println("Program Start");
+  Serial.println("--------------------");
+
+  pinMode(transistorPin1, OUTPUT);
+  pinMode(transistorPin2, OUTPUT);
+  pinMode(transistorPin3, OUTPUT);
+
+  digitalWrite(transistorPin1, LOW);
+  digitalWrite(transistorPin2, LOW);
+  digitalWrite(transistorPin3, LOW);
 }
 
-void loop()
-{
-	digitalWrite(LED_SAFETY, HIGH);
-	digitalWrite(SAFETY_OUT, HIGH);
-  
-  	digitalWrite(LED_SWITCH, HIGH);
-    digitalWrite(SWITCH_OUT, HIGH);
-  
-  	digitalWrite(LED_READY_TO_FIRE, LOW);
-  	
-  	if(!digitalRead(SAFETY_IN) == LOW)
-    {
-    	Serial.println("SAFETY ON");
-      	delay(100);
-      	return;
+String messageBuffer = "";
+String message = "";
+
+void loop() {
+  while (BTSerial.available() > 0) {
+    char data = (char)BTSerial.read();
+    Serial.print(data);  // Print each received character for debugging
+    messageBuffer += data;
+    if (data == ';') {
+      // Keep only the last two characters
+      if (messageBuffer.length() > 2) {
+        message = messageBuffer.substring(messageBuffer.length() - 2);
+      } else {
+        message = messageBuffer;
+      }
+      messageBuffer = "";
+      Serial.print("\nReceived message: ");
+      Serial.println(message);  // Print the full received message
+      BTSerial.println("You sent" + message);
+      if (message == "1;") {
+        digitalWrite(transistorPin1, HIGH);
+        Serial.println("firing 1");
+        BTSerial.println("firing 1");
+        delay(5000);
+        digitalWrite(transistorPin1, LOW);
+        Serial.println("1 has fired");
+        BTSerial.println("1 has fired");
+      }
+      else if (message == "2;") {
+        digitalWrite(transistorPin2, HIGH);
+        Serial.println("firing 2");
+        BTSerial.println("firing 2");
+        delay(5000);
+        digitalWrite(transistorPin2, LOW);
+        Serial.println("2 has fired");
+        BTSerial.println("2 has fired");
+      }
+      else if (message == "3;") {
+        digitalWrite(transistorPin3, HIGH);
+        Serial.println("firing 3");
+        BTSerial.println("firing 3");
+        delay(5000);
+        digitalWrite(transistorPin3, LOW);
+        Serial.println("3 has fired");
+        BTSerial.println("3 has fired");
+      }
+      message = ""; // Clear the message after processing
     }
- 	Serial.println("SAFETY OFF");
-	digitalWrite(LED_SAFETY, LOW);
-	
-  	if(!digitalRead(SWITCH_IN) == LOW)
-    {
-    	Serial.println("SWITCH OFF");
-    	delay(100);
-      	return;
-    }
-  	Serial.println("SWITCH ON");
-  	digitalWrite(LED_SWITCH, LOW);
-  
-    delay(100);
+  }
 }
